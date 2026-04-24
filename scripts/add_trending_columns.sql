d463975b204ad3132b8f5208ec3f30849e133f85
ym8895 <misc8896@gmail.com>
2026-04-24 15:02:12 +0530

    Fix: Simplified SQL for trending columns

--- scripts/add_trending_columns.sql
+++ scripts/add_trending_columns.sql
@@ -3,11 +3,5 @@ ALTER TABLE book_views
 ADD COLUMN book_author TEXT,
 ADD COLUMN cover_url TEXT;
 
--- Update existing records where we have this data in book_content
-UPDATE book_views bv
-SET book_author = bc.author, cover_url = bc.cover_url
-FROM book_content bc
-WHERE bv.book_id = bc.book_id;
-
 -- Create index for faster queries
 CREATE INDEX IF NOT EXISTS idx_book_views_book_id ON book_views(book_id);
